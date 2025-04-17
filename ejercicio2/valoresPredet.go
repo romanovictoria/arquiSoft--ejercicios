966c822cfe43e9ca2b80bfdf64d0c608daf51470
romanovictoria <vicctoriarom@gmail.com>
2025-04-17 15:38:25 -0300

    se agrego ejercicio de valores predeterminados

---
+++ ejercicio2/valoresPredet.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//valores predeterminados
+	var (
+		defaultInt     int
+		defaultUint    uint
+		defaultFloat32 float32
+		defaultbool    bool
+		defaultstring  string
+	)
+	fmt.Println(defaultInt, defaultUint, defaultFloat32, defaultbool, defaultstring)
+}
